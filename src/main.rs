3d454b809ad0b9a1172f06b758ac5410e85957f9
Arthur Coruble <arthur.coruble@dashlane.com>
2022-05-14 22:47:17 +0300

    First dirty working alias manager version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env;
+use std::fs;
+use std::io::Write;
 use std::process;
 
 mod cli_parser;
+
+const ZSHRC_FILE_PATH: &str = "/Users/acoruble/.zshrc";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,5 +14,33 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{} {}", config.alias_command, config.alias_trigger);
+    let mut shell_configuration_file = fs::OpenOptions::new()
+        .append(true)
+        .open(ZSHRC_FILE_PATH)
+        .unwrap_or_else(|err| {
+            eprintln!(
+                "Error opening shell configuration file at {}.
+                The error we got was: {}",
+                ZSHRC_FILE_PATH, err
+            );
+            process::exit(1);
+        });
+
+    shell_configuration_file
+        .write(
+            format!(
+                "alias {}=\"{}\"\n",
+                config.alias_trigger, config.alias_command
+            )
+            .as_bytes(),
+        )
+        .unwrap_or_else(|err| {
+            eprintln!(
+                "Error appending alias to shell configuration file.
+            The error we got was: {}",
+                err
+            );
+            process::exit(1);
+        });
+    process::exit(0)
 }
