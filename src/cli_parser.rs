3bf6f6518c9ae7d701c47830393cc740e9c59f9b
Arthur Coruble <arthur.coruble@dashlane.com>
2022-05-26 15:41:28 +0300

    feat: add support for '--help' argument & better error handling

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -1,19 +1,62 @@
+use std::env;
+
+const SHELL_CONFIGURATION_FILE_PATH_ENVIRONMENT_VARIABLE: &str = "SHELL_CONFIGURATION_FILE_PATH";
+const HELP_ARGUMENT: &str = "--help";
+
 pub struct CLIConfig {
     pub alias_command: String,
     pub alias_trigger: String,
+    pub shell_configuration_file_path: String,
 }
 
-impl CLIConfig {
-    pub fn new(args: &Vec<String>) -> Result<CLIConfig, &'static str> {
-        if args.len() != 3 {
-            return Err("Incorrect number of arguments passed to aliasmanager");
-        };
-        let alias_trigger = args[1].clone();
-        let alias_command = args[2].clone();
-        Ok(CLIConfig {
-            alias_command,
-            alias_trigger,
-        })
+pub enum CLIConfigStatus {
+    HELP,
+    VALID(CLIConfig),
+    INVALID(String)
+}
+
+impl CLIConfigStatus {
+    pub fn new(args: &Vec<String>) -> CLIConfigStatus {
+        let help_string = String::from(HELP_ARGUMENT);
+        match args.len() {
+            2 => {
+                if args[1] != help_string {
+                    return CLIConfigStatus::INVALID(String::from("Unknown argument passed, see usage with 'aliasmanager --help'"))
+                }
+                return CLIConfigStatus::HELP;
+            },
+            3 => {
+                return match CLIConfig::new(args) {
+                    Ok(config) => CLIConfigStatus::VALID(config),
+                    Err(error) => CLIConfigStatus::INVALID(error)
+                };
+                
+            },
+            _ => {
+                return CLIConfigStatus::INVALID(String::from("Wrong number of arguments, see usage with 'aliasmanager --help'"))
+            }
+        }
     }
 }
 
+impl CLIConfig {
+    pub fn new(args: &Vec<String>) -> Result<CLIConfig, String> {
+        let alias_trigger = args[1].clone();
+        let alias_command = args[2].clone();
+
+        let shell_configuration_file_path =
+            env::var(SHELL_CONFIGURATION_FILE_PATH_ENVIRONMENT_VARIABLE);
+        let shell_configuration_file_path = match shell_configuration_file_path {
+            Ok(path) => path,
+            Err(_error) => {
+                return Err(String::from("Error reading SHELL_CONFIGURATION_FILE_PATH environment variable"))
+            }
+        };
+
+        Ok(CLIConfig {
+            alias_command,
+            alias_trigger,
+            shell_configuration_file_path,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,67 @@ use std::io::Write;
 use std::process;
 
 mod cli_parser;
+use cli_parser::CLIConfigStatus;
+use cli_parser::CLIConfig;
 
-const ZSHRC_FILE_PATH: &str = "/Users/acoruble/.zshrc";
+const HELP_MESSAGE: &str = "
+AliasManager
+    A tiny utilitary CLI tool to quickly create new aliases in your shell config file.
+    Please note that this programs uses the `SHELL_CONFIGURATION_FILE_PATH` environment
+    variable to determine where to write the alias.
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = cli_parser::CLIConfig::new(&args).unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        process::exit(1);
-    });
+USAGE:
+    aliasmanager [--OPTIONS] <trigger> <command>
+                
+OPTIONS:
+    --help: prints this help message
+";
 
-    let mut shell_configuration_file = fs::OpenOptions::new()
+fn run(config: CLIConfig) {
+    let alias_to_write = format!(
+        "alias {}=\"{}\"\n",
+        config.alias_trigger, config.alias_command
+    );
+
+    let _shell_configuration_file = fs::OpenOptions::new()
         .append(true)
-        .open(ZSHRC_FILE_PATH)
+        .open(&config.shell_configuration_file_path)
         .unwrap_or_else(|err| {
             eprintln!(
                 "Error opening shell configuration file at {}.
                 The error we got was: {}",
-                ZSHRC_FILE_PATH, err
+                config.shell_configuration_file_path, err
             );
             process::exit(1);
-        });
-
-    shell_configuration_file
-        .write(
-            format!(
-                "alias {}=\"{}\"\n",
-                config.alias_trigger, config.alias_command
-            )
-            .as_bytes(),
-        )
+        })
+        .write(alias_to_write.as_bytes())
         .unwrap_or_else(|err| {
             eprintln!(
                 "Error appending alias to shell configuration file.
-            The error we got was: {}",
+                The error we got was: {}",
                 err
             );
             process::exit(1);
         });
+
     process::exit(0)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config_status = CLIConfigStatus::new(&args);
+
+    match config_status {
+        CLIConfigStatus::VALID(config) => {
+            run(config);
+        },
+        CLIConfigStatus::INVALID(error) => {
+            eprintln!("Something went wrong, here is the error:\n{}", error);
+            process::exit(1);
+        },
+        CLIConfigStatus::HELP => {
+            println!("{}", HELP_MESSAGE);
+            process::exit(0);
+        }
+    };
 }
