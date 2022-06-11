552cdf8e83f82c224d97ac97c8dc516e011bec49
Arthur Coruble <arthur.coruble@dashlane.com>
2022-06-11 17:21:59 +0200

    refactor: improve overall syntax & string usages

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -9,54 +9,42 @@ pub struct CLIConfigData {
     pub shell_configuration_file_path: String,
 }
 
-pub enum CLIConfig {
+pub enum CLIConfig<'a> {
     Help,
     Valid(CLIConfigData),
-    Invalid(String),
+    Invalid(&'a str),
 }
 
-impl CLIConfig {
+impl CLIConfig<'_> {
     pub fn new(args: &[String]) -> CLIConfig {
-        let help_string = String::from(HELP_ARGUMENT);
         match args.len() {
-            2 => {
-                if args[1] != help_string {
-                    return CLIConfig::Invalid(String::from(
-                        "Unknown argument passed, see usage with 'aliasmanager --help'",
-                    ));
-                }
-                CLIConfig::Help
+            2 if args[1].as_str() != HELP_ARGUMENT => {
+                CLIConfig::Invalid("Unknown argument passed, see usage with 'aliasmanager --help'")
             }
+            2 => CLIConfig::Help,
             3 => match CLIConfigData::new(args) {
                 Ok(config) => CLIConfig::Valid(config),
                 Err(error) => CLIConfig::Invalid(error),
             },
-            _ => CLIConfig::Invalid(String::from(
+            _ => CLIConfig::Invalid(
                 "Wrong number of arguments, see usage with 'aliasmanager --help'",
-            )),
+            ),
         }
     }
 }
 
 impl CLIConfigData {
-    pub fn new(args: &[String]) -> Result<CLIConfigData, String> {
+    pub fn new(args: &[String]) -> Result<CLIConfigData, &str> {
         let alias_trigger = args[1].clone();
         let alias_command = args[2].clone();
 
-        let shell_configuration_file_path =
-            env::var(SHELL_CONFIGURATION_FILE_PATH_ENVIRONMENT_VARIABLE);
-        let shell_configuration_file_path = match shell_configuration_file_path {
-            Ok(path) => path,
-            Err(_error) => {
-                String::from("Error reading SHELL_CONFIGURATION_FILE_PATH environment variable")
-            }
-        };
-
-        Ok(CLIConfigData {
-            alias_command,
-            alias_trigger,
-            shell_configuration_file_path,
-        })
+        env::var(SHELL_CONFIGURATION_FILE_PATH_ENVIRONMENT_VARIABLE)
+            .map(|path| CLIConfigData {
+                alias_command,
+                alias_trigger,
+                shell_configuration_file_path: path,
+            })
+            .map_err(|_e| "Error reading SHELL_CONFIGURATION_FILE_PATH environment variable")
     }
 }
 
@@ -95,5 +83,42 @@ mod tests {
             CLIConfig::new(&args),
             CLIConfig::Invalid(_error_string)
         ))
+    }
+
+    #[test]
+    fn test_valid_cli_configuration_without_env_variable() {
+        let args = vec![
+            String::from("program_path"),
+            String::from("gco"),
+            String::from("git checkout"),
+        ];
+        let _error_string =
+            String::from("Error reading SHELL_CONFIGURATION_FILE_PATH environment variable");
+        assert!(matches!(
+            CLIConfig::new(&args),
+            CLIConfig::Invalid(_error_string)
+        ));
+    }
+
+    #[test]
+    fn test_valid_cli_configuration_with_env_variable() {
+        env::set_var(
+            SHELL_CONFIGURATION_FILE_PATH_ENVIRONMENT_VARIABLE,
+            "~/.zshrc",
+        );
+        let args = vec![
+            String::from("program_path"),
+            String::from("gco"),
+            String::from("git checkout"),
+        ];
+        let _cli_config_data = CLIConfigData {
+            alias_command: String::from("git checkout"),
+            alias_trigger: String::from("gco"),
+            shell_configuration_file_path: String::from("~/.zshrc"),
+        };
+        assert!(matches!(
+            CLIConfig::new(&args),
+            CLIConfig::Valid(_cli_config_data)
+        ));
     }
 }
