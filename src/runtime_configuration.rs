83d3857266435fc21c2b373000b18c42838329fd
Arthur Coruble <arthur.coruble@dashlane.com>
2022-07-13 17:35:34 +0200

    refactor(config): extract file creation logic into standalone function

--- src/runtime_configuration.rs
+++ src/runtime_configuration.rs
@@ -5,10 +5,10 @@ use std::{
 };
 
 use config::{Config, ConfigError, File};
-use directories::{BaseDirs, ProjectDirs};
+use directories::ProjectDirs;
 use serde::Deserialize;
 
-const RUNTIME_CONFIGURATION_FILENAME: &str = ".aliasmanagerrc.toml";
+const RUNTIME_CONFIGURATION_FILENAME: &str = ".aliasmanager_rc.toml";
 const DEFAULT_CONFIG_CONTENT: &str = "[aliasmanager]
 shell_file_path = \"$HOME/.zshrc\"
 ";
@@ -37,6 +37,29 @@ pub struct RuntimeConfiguration {
     pub aliasmanager: Aliasmanager,
 }
 
+fn create_new_file(path: &Path, permissions: &str) -> Result<std::fs::File, String> {
+    if permissions.len() != 2 {
+        Err("Wrong parameter for create_new_file".to_string())
+    } else {
+        let mut opener = OpenOptions::new();
+        if !path.exists() {
+            opener.create(true);
+        }
+        for c in permissions.chars() {
+            match c {
+                'r' => opener.read(true),
+                'w' => opener.write(true),
+                _ => {
+                    return Err("Wrong permissions string parameter for create_new_file".to_string())
+                }
+            };
+        }
+        opener
+            .open(&path)
+            .map_err(|err| format!("Could not create file with error message:\n {}", err))
+    }
+}
+
 impl RuntimeConfiguration {
     pub fn new() -> Result<Self, ConfigError> {
         match ProjectDirs::from("dev", "acoruble", "aliasmanager") {
@@ -44,35 +67,22 @@ impl RuntimeConfiguration {
                 "Could not determine OS default settings path".to_string(),
             )),
             Some(project_dirs) => {
-                dbg!(BaseDirs::new());
-                let config_dir = project_dirs.config_dir();
-                let config_dir = Path::new(config_dir);
+                let config_dir = Path::new(project_dirs.config_dir());
                 let file_path = config_dir.join(RUNTIME_CONFIGURATION_FILENAME);
-                let config_file_exists = file_path.exists();
-                if !config_file_exists {
+                if !file_path.exists() {
                     if !config_dir.exists() {
                         println!("> Config dir does not exist, it will now be created");
                         create_dir_all(config_dir);
                     }
-                    let mut file = OpenOptions::new()
-                        .create(true)
-                        .read(true)
-                        .write(true)
-                        .open(&file_path)
-                        .unwrap();
-                    file.write(DEFAULT_CONFIG_CONTENT.as_bytes());
-                    Config::builder()
-                        .add_source(File::with_name(file_path.as_os_str().to_str().unwrap()))
-                        .build()
+                    create_new_file(&file_path, "rw")
                         .unwrap()
-                        .try_deserialize()
-                } else {
-                    Config::builder()
-                        .add_source(File::with_name(file_path.as_os_str().to_str().unwrap()))
-                        .build()
-                        .unwrap()
-                        .try_deserialize()
+                        .write(DEFAULT_CONFIG_CONTENT.as_bytes());
                 }
+                Config::builder()
+                    .add_source(File::with_name(file_path.as_os_str().to_str().unwrap()))
+                    .build()
+                    .unwrap()
+                    .try_deserialize()
             }
         }
     }
